edb9f815f96282d0db7a10a1d6e819211762842f
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-07-01 22:36:52 -0300

    changing component folders for exercises, exercise 16

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,7 +3,7 @@
 //import Exercise13 from "./exercises/Exercise13";
 //import Exercise14 from "./exercises/Exercise14";
 //import Exercise15 from "./exercises/Exercise15";
-import Unicafe16 from "./components/unicafe/exercises16/app";
+import Unicafe16 from "./exercises/unicafe/exercises16/app";
 
 const App = () => {
   return <Unicafe16 />;
