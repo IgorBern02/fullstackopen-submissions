00b04e06fbb88a60737f0924338c2f89bd3ef51c
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-07-01 22:27:58 -0300

    Added exercises 16 unicafe from part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,10 +2,11 @@
 //import Exercise12 from "./exercises/Exercise12";
 //import Exercise13 from "./exercises/Exercise13";
 //import Exercise14 from "./exercises/Exercise14";
-import Exercise15 from "./exercises/Exercise15";
+//import Exercise15 from "./exercises/Exercise15";
+import Unicafe16 from "./components/unicafe/exercises16/app";
 
 const App = () => {
-  return <Exercise15 />;
+  return <Unicafe16 />;
 };
 
 export default App;
