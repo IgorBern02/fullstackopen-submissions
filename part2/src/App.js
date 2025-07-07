97be991da8d2181b6e42f6996e2151364d940a05
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-07-07 15:57:32 -0300

    Added exercise 24 from part 2

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,8 +1,9 @@
 //import Exercise21 from "./exercises/courseinformation/exercise21";
-import Exercise22 from "./exercises/courseinformation/exercise22";
+//import Exercise22 from "./exercises/courseinformation/exercise22";
+import Exercise24 from "./exercises/courseinformation/exercise24";
 
 const App = () => {
-  return <Exercise22 />;
+  return <Exercise24 />;
 };
 
 export default App;
